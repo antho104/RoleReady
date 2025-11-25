826f3530af09bfbba955f340ab065378e8a0c9c3
antho104 <anthony.luke.papier@gmail.com>
2025-11-25 14:45:18 +0000

    force cdk

--- backend/tests/test_handler.py
+++ backend/tests/test_handler.py
@@ -3,5 +3,5 @@ from handler import handler
 def test_handler_returns_hello():
     response = handler({}, {})
     assert response["statusCode"] == 200
-    assert response["body"] == "Hello from Lambda!"
+    assert response["body"] == "Hello from Lambda test!"
 
