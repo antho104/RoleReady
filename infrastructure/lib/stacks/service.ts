7485b6e672a6330a87fddb96651ea946bebc89fd
antho104 <anthony.luke.papier@gmail.com>
2025-12-11 14:24:14 +0000

    Added cognito user pool - Test apigateway with lambda integration - Backed by cognito authencation for each enpoint

--- infrastructure/lib/stacks/service.ts
+++ infrastructure/lib/stacks/service.ts
@@ -4,12 +4,63 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 
 
 export class ServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     
+    const userPool = new cognito.UserPool(this, 'InterviewQuestionBankUserPool', {
+      userPoolName: 'interview-question-bank-users',
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: false,
+        },
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+        requireSymbols: false,
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    const userPoolClient = userPool.addClient('WebAppClient', {
+      userPoolClientName: 'web-app-client',
+      authFlows: {
+        userPassword: true,
+        userSrp: true,
+      },
+      preventUserExistenceErrors: true,
+    });
+
+    new cdk.CfnOutput(this, 'UserPoolId', {
+      value: userPool.userPoolId,
+      description: 'Cognito User Pool ID',
+    });
+
+    new cdk.CfnOutput(this, 'UserPoolClientId', {
+      value: userPoolClient.userPoolClientId,
+      description: 'Cognito User Pool Client ID',
+    });
+
+    new cdk.CfnOutput(this, 'CognitoRegion', {
+      value: this.region,
+      description: 'AWS Region for Cognito',
+    });
+
     const frontendS3 = new s3.Bucket(this, 'FrontendBucket', {
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       publicReadAccess: false,
@@ -41,26 +92,81 @@ export class ServiceStack extends cdk.Stack {
     const helloFn = new lambda.Function(this, 'HelloFunction', {
       runtime: lambda.Runtime.PYTHON_3_11,
       handler: 'handler.handler',
-      code: lambda.Code.fromAsset("../backend")
+      code: lambda.Code.fromAsset("../backend"),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
     });
 
     // Grant the Lambda function read/write permissions to the table
     table.grantReadWriteData(helloFn);
 
+    const lambdaIntegration = new apigw.LambdaIntegration(helloFn);
+
+    const cognitoAuthorizer = new apigw.CognitoUserPoolsAuthorizer(this, 'CognitoAuthorizer', {
+      cognitoUserPools: [userPool],
+      authorizerName: 'CognitoAuthorizer',
+      identitySource: 'method.request.header.Authorization',
+    });
+
     // Public HTTP endpoint using API Gateway
     const api = new apigw.LambdaRestApi(this, 'TestApi', {
       handler: helloFn,
       proxy: false,
-      description: 'Simple test API to verify CDK deployments',
+      description: 'Interview Question Bank API',
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+        allowMethods: apigw.Cors.ALL_METHODS,
+        allowHeaders: [
+          'Content-Type',
+          'X-Amz-Date',
+          'Authorization',
+          'X-Api-Key',
+          'X-Amz-Security-Token',
+        ],
+        allowCredentials: true,
+      },
     });
 
-    const test = api.root.addResource('testing')
-    test.addMethod('GET')
+    const test = api.root.addResource('testing');
+    test.addMethod('GET', lambdaIntegration);
+
+    const questions = api.root.addResource('questions');
+
+    questions.addMethod('GET', lambdaIntegration, {
+      authorizer: cognitoAuthorizer,
+      authorizationType: apigw.AuthorizationType.COGNITO,
+    });
+
+    questions.addMethod('POST', lambdaIntegration, {
+      authorizer: cognitoAuthorizer,
+      authorizationType: apigw.AuthorizationType.COGNITO,
+    });
+
+    const questionById = questions.addResource('{id}');
+    questionById.addMethod('GET', lambdaIntegration, {
+      authorizer: cognitoAuthorizer,
+      authorizationType: apigw.AuthorizationType.COGNITO,
+    });
+
+    questionById.addMethod('PUT', lambdaIntegration, {
+      authorizer: cognitoAuthorizer,
+      authorizationType: apigw.AuthorizationType.COGNITO,
+    });
+
+    questionById.addMethod('DELETE', lambdaIntegration, {
+      authorizer: cognitoAuthorizer,
+      authorizationType: apigw.AuthorizationType.COGNITO,
+    });
 
     // Output the URL so you can curl it after deploy
     new cdk.CfnOutput(this, 'ApiUrl', {
       value: api.url,
       description: 'Invoke this URL to test the deployed Lambda',
+    });
+
+    new cdk.CfnOutput(this, 'QuestionsEndpoint', {
+      value: `${api.url}questions`,
     });
   }
 }
